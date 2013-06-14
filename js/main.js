d6949185d81d5dbd19ce004a37fcf1fb044be9c4
Graham Smith <graham@thingie.co.uk>
2013-06-14 20:31:29 +0100

    Added the Game Loop

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ var canvas  = document.getElementById('super-js-adventure'),
     width   = 256,
     height  = 224;
 
+
+function init() {
+    // Initialise the game!
+}
+
+function main() {
+    // Here's where we handle all the input, logic and drawing to the screen per frame.
+
+    // call itself by requesting the next animation frame, and so begin the endless loop
+    requestAnimationFrame(main);
+}
+
+
+// Initialise
+init();
+
+// Start the loop!
+requestAnimationFrame(main);
