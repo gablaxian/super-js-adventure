b27429e2e60d8603034260aab6f9e4fd31ae6519
Graham Smith <graham@thingie.co.uk>
2013-06-15 12:02:25 +0100

    We now have link being drawn to the canvas

--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@
 var canvas  = document.getElementById('super-js-adventure'),
     ctx     = canvas.getContext('2d'),
     width   = 256,
-    height  = 224;
+    height  = 224,
+    link    = new Image();
 
 
 function init() {
     // Initialise the game!
+    link.src = 'images/link.png';
 }
 
 function main() {
     // Here's where we handle all the input, logic and drawing to the screen per frame.
+    ctx.clearRect(0, 0, 256, 224);
+
+    ctx.drawImage(link, 20, 20);
 
     // call itself by requesting the next animation frame, and so begin the endless loop
     requestAnimationFrame(main);
