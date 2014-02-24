523c9d2ff4e2537b38de67b8218f97376087ec56
Graham Smith <graham@thingie.co.uk>
2014-02-24 21:27:58 +0000

    Frame Rate Independence for Link

--- js/main.js
+++ js/main.js
@@ -4,8 +4,14 @@ var canvas  = document.getElementById('super-js-adventure'),
     ctx     = canvas.getContext('2d'),
     width   = 256,
     height  = 224,
+    mainLoop = null,
     key     = [0,0,0,0,0,0,0,0], // left, right, up, down, attack, use item, start, select
     link    = new Image(),
+    gutter  = 2,
+    lastTime = 0,
+    animationUpdateTime = 0,
+    timeSinceLastFrameSwap = 0,
+    elapsed = 0,
     player  = {
         x : 0,
         y : 0
@@ -23,8 +29,13 @@ function init() {
     player.sequence     = [3,4,5,6,5,4,3,2,1,0,1,2];
     player.sequenceIdx  = 0;
 
+    player.fps = 16;
+    player.animationUpdateTime = 1000 / player.fps;
+
     // Setup the Input
     Input.init();
+
+    lastTime = window.performance.now();
 
     // scale_canvas(3);
 }
@@ -46,35 +57,47 @@ var frame_count = 0;
 
 function main() {
     /** Here's where we handle all the input, logic and drawing to the screen per frame. **/
+    var now     = window.performance.now(),
+        elapsed = (now - lastTime);
+
+    lastTime = now;
 
     // Clear the screen
     ctx.clearRect(0, 0, 256, 224);
 
+    var speed = 2;
+
     // Handle the Input
     if (key[2])
-        player.y -= 4;
+        player.y -= speed;
     if( key[3])
-        player.y += 4;
+        player.y += speed;
     if( key[0])
-        player.x -= 4;
+        player.x -= speed;
     if( key[1])
-        player.x += 4;
+        player.x += speed;
 
     // loop through the walking sequence
-    var gutter      = 2;
-    var spritePos   = ( player.sequence[player.sequenceIdx] * gutter ) + ( player.sequence[player.sequenceIdx] * 16 );
+    var spritePos = ( player.sequence[player.sequenceIdx] * gutter ) + ( player.sequence[player.sequenceIdx] * 16 );
+
+    timeSinceLastFrameSwap += elapsed;
+
+    // half the animation speed
+    if( timeSinceLastFrameSwap > player.animationUpdateTime ) {
+        if( player.sequenceIdx < player.sequence.length - 1 )
+            player.sequenceIdx += 1;
+        else
+            player.sequenceIdx = 0;
+
+        timeSinceLastFrameSwap = 0;
+    }
 
     ctx.drawImage(link, spritePos, 0, 16, 25, player.x, player.y, 16, 25);
 
-    if( player.sequenceIdx < player.sequence.length - 1 )
-        player.sequenceIdx += 1;
-    else
-        player.sequenceIdx = 0;
+    frame_count++;
 
     // call itself by requesting the next animation frame, and so begin the endless loop
-    requestAnimationFrame(main);
-
-    frame_count += 1;
+    mainLoop = requestAnimationFrame(main);
 }
 
 
