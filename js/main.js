c07ff4b944ead680b55a7b814ded0169af52e721
Graham Smith <g@blaxian.com>
2015-09-28 20:35:59 +0100

    A level built with placeholder cells and rudimentary tile-based collision

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 
 // get that canvas!
-var canvas  = document.getElementById('super-js-adventure'),
-    ctx     = canvas.getContext('2d'),
-    width   = 256,
-    height  = 224,
-    mainLoop = null,
-    key     = [0,0,0,0,0,0,0,0], // left, right, up, down, attack, use item, start, select
-    link    = null, // our intrepid hero
-    gutter  = 2,
-    lastTime = 0,
-    animationUpdateTime = 0,
-    timeSinceLastFrameSwap = 0,
-    elapsed = 0,
-    player  = {
-        x : 0,
-        y : 0
-    };
+var canvas  = document.getElementById('super-js-adventure');
+var ctx     = canvas.getContext('2d');
+var width   = 256;
+var height  = 224;
+var mainLoop = null;
+var key     = [0,0,0,0,0,0,0,0]; // left, right, up, down, attack, use item, start, select
+var link    = null; // our intrepid hero
+var gutter  = 2;
+var lastTime = 0;
+var animationUpdateTime = 0;
+var timeSinceLastFrameSwap = 0;
+var elapsed = 0;
+var level;
 
 function Link(x, y) {
 
@@ -25,7 +22,10 @@ function Link(x, y) {
     this.x = x;
     this.y = y;
 
-    this.fps = 16;
+    this.width = 16;
+    this.height = 25;
+
+    this.fps = 60;
     this.animationUpdateTime = 1000 / this.fps;
 
     this.timeSinceLastFrameSwap = 0;
@@ -46,7 +46,6 @@ function Link(x, y) {
     this.update = function(elapsed) {
         this.timeSinceLastFrameSwap += elapsed;
 
-        // half the animation speed
         if( this.timeSinceLastFrameSwap > this.animationUpdateTime ) {
 
             var seq = this.moving ? 'walk-' : 'stand-';
@@ -62,8 +61,8 @@ function Link(x, y) {
             var col = currentSequence[this.sequenceIdx] % 7;
             var row = Math.floor( currentSequence[this.sequenceIdx] / 7 );
 
-            this.offsetX = ( col * 16 ) + ( col * gutter );
-            this.offsetY = row * 25;
+            this.sliceX = ( col * 16 ) + ( col * gutter );
+            this.sliceY = row * 25;
 
             this.timeSinceLastFrameSwap = 0;
         }
@@ -75,11 +74,13 @@ function Link(x, y) {
 
         ctx.save();
 
-        ctx.translate(this.x, this.y)
+        ctx.translate(this.x, this.y);
+        ctx.translate(8, 0);
         ctx.scale(scaleX, 1);
-        ctx.drawImage(this.img, this.offsetX, this.offsetY, 16, 25, -8, 0, 16, 25);
+        ctx.drawImage(this.img, this.sliceX, this.sliceY, 16, 25, -8, 0, 16, 25);
 
         ctx.restore();
+
     }
 }
 
@@ -92,20 +93,100 @@ function init() {
 
     lastTime = window.performance.now();
 
-    // scale_canvas(3);
+    zoom(2);
+
+    level = world.level;
 }
 
-function scale_canvas(s) {
-    var css = '';
-        var prefixes = ['webkit','moz','o','ms'];
-        for (var i=0; i<prefixes.length; i++)
-        {
-            var prefix = prefixes[i];
-            css += '-'+prefix+'-transform: scale('+s+','+s+');';
-            css += '-'+prefix+'-transform-origin: 0 0;'
-        };
-        canvas.style.cssText = css;
-        canvas.parentNode.style.cssText = 'width:'+width*s+'px;height:'+height*s+'px;';
+function zoom(s) {
+    canvas.style.cssText = 'width:'+width*s+'px;height:'+height*s+'px;';
+    canvas.parentNode.style.cssText = 'width:'+width*s+'px;height:'+height*s+'px;';
+}
+
+function drawBackground() {
+
+    // check that the world exists.
+    if( world ) {
+
+        for (var row = 0; row < (height/8); row++) {
+            for (var col = 0; col < (width/8); col++) {
+                var tile = level[row][col];
+                var x = (col * 8);
+                var y = (row * 8);
+
+                if( tile == 0 ) {
+                    ctx.fillStyle = '#000';
+                    ctx.strokeStyle = '#000';
+                }
+                else if( tile == 1 ) {
+                    ctx.fillStyle = '#85724E';
+                    ctx.strokeStyle = '#68583A';
+                }
+                else if( tile == 2 ) {
+                    ctx.fillStyle = '#f88';
+                    ctx.strokeStyle = '#f00';
+                }
+
+                ctx.fillRect(x, y, 8, 8);
+                ctx.strokeRect(x+0.5, y+0.5, 8, 8);
+            }
+        }
+
+    }
+
+}
+
+function checkCollisions() {
+    if( link.x < 0 ) {
+        link.x = 0;
+    }
+    if( link.y < 0 ) {
+        link.y = 0;
+    }
+    if( (link.x + link.width) > width ) {
+        link.x = (width - link.width);
+    }
+    if( (link.y + link.height) > height ) {
+        link.y = (height - link.height);
+    }
+
+    if( key[2] ) { // up
+        var topLeftCol = Math.floor(link.x / 8);
+        var topRightCol = Math.floor((link.x+link.width-1) / 8);
+        var row = Math.floor((link.y+9) / 8); // same for topleft and topright
+
+        // now get the cells for each corner and check 'em!
+        if( level[row][topLeftCol] == 2 || level[row][topRightCol] == 2 ) {
+            link.y = (row * 8);
+        }
+    }
+    if( key[3]) { // down
+        var bottomLeftCol = Math.floor(link.x / 8);
+        var bottomRightCol = Math.floor((link.x+link.width-1) / 8);
+        var row = Math.floor((link.y+link.height) / 8);
+
+        if( level[row][bottomLeftCol] == 2 || level[row][bottomRightCol] == 2 ) {
+            link.y = (row * 8) - link.height;
+        }
+    }
+    if( key[0]) { // left
+        var col = Math.floor(link.x / 8);
+        var topLeftRow = Math.floor((link.y+9) / 8);
+        var bottomLeftRow = Math.floor((link.y+link.height-1) / 8);
+
+        if( level[topLeftRow][col] == 2 || level[bottomLeftRow][col] == 2 ) {
+            link.x = (col * 8) + 8;
+        }
+    }
+    if( key[1]) { //right
+        var col = Math.floor((link.x+link.width) / 8);
+        var topRightRow = Math.floor((link.y+9) / 8);
+        var bottomRightRow = Math.floor((link.y+link.height-1) / 8);
+
+        if( level[topRightRow][col] == 2 || level[bottomRightRow][col] == 2 ) {
+            link.x = (col * 8) - link.width;
+        }
+    }
 }
 
 function main() {
@@ -117,6 +198,8 @@ function main() {
 
     // Clear the screen
     ctx.clearRect(0, 0, 256, 224);
+
+    drawBackground();
 
     var speed = 2;
 
@@ -146,10 +229,12 @@ function main() {
 
     link.update(elapsed);
 
+    checkCollisions();
+
     link.draw();
 
     // call itself by requesting the next animation frame, and so begin the endless loop
-    mainLoop = requestAnimationFrame(main);
+    requestAnimationFrame(main);
 }
 
 
